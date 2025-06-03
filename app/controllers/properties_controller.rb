6f7ffdc45d8f24ac2f76fcf872442724174349a2
John Paul Ricarse <johnpauliloilo@gmail.com>
2025-06-03 21:10:15 +1200

    adds support for editing and destroying properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,17 +1,20 @@
 class PropertiesController < ApplicationController
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :find_property, only: [:show, :edit, :update, :destroy]
 
   def index
     @properties = Property.all.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def show
-    @property = Property.find(params[:id])
   end
 
   def new
     @property = Property.new
+  end
+
+  def edit
   end
 
   def create
@@ -21,12 +24,31 @@ class PropertiesController < ApplicationController
       flash[:notice] = "Property successfully created."
       redirect_to property_path(@property)
     else
-      flash[:alert] = "Failed to create property."
+      flash[:error] = "Failed to create property."
       render :new
     end
   end
 
+  def update
+    if @property.update(property_params)
+      flash[:notice] = "Property successfully updated."
+      redirect_to property_path(@property)
+    else
+      flash[:error] = "Failed to edit property."
+    end
+  end
+
+  def destroy
+    @property.destroy
+    flash[:notice] = "Property successfully destroyed."
+    redirect_to properties_path
+  end
+
   private
+
+  def find_property
+    @property = Property.find(params[:id])
+  end
 
   def property_params
     params.require(:property).permit(:name, :description, :address)
