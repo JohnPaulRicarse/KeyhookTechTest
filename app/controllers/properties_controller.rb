be3b25594240bf92749cfa637e8046a968a6db76
John Paul Ricarse <johnpauliloilo@gmail.com>
2025-06-03 20:01:54 +1200

    add support for creating new property

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,10 +3,32 @@ class PropertiesController < ApplicationController
   allow_browser versions: :modern
 
   def index
-    @properties = Property.all.order(:created_at).page(params[:page]).per(10)
+    @properties = Property.all.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def show
     @property = Property.find(params[:id])
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+
+    if @property.save
+      flash[:notice] = "Property successfully created."
+      redirect_to property_path(@property)
+    else
+      flash[:alert] = "Failed to create property."
+      render :new
+    end
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:name, :description, :address)
   end
 end
